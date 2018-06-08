2435af0d78c9627f8d654ec2ac4ea8312305d006
Jimmy Chang <jimmyfchang94@gmail.com>
2018-06-07 22:55:06 -0700

    Initial scaffolding and creating a file functionality

---
+++ Lab03-Word-Guess-Game/Lab03-Word-Guess-Game-Unit-Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab03_Word_Guess_Game;
+
+namespace Lab03_Word_Guess_Game_Unit_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Lab03-Word-Guess-Game/Lab03-Word-Guess-Game/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lab03_Word_Guess_Game
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../storedWords.txt";
+            Console.WriteLine("Hello World!");
+            CreateFile(path);
+        }
+
+        /// <summary>
+        /// This method creates a new file if there is none. It then writes some words into the file.
+        /// It requires a path to indicate where it should look for/make a file.
+        /// </summary>
+        /// <param name="path"></param>
+        static void CreateFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter fileNew = new StreamWriter(path))
+                {
+                    fileNew.WriteLine("cat");
+                    fileNew.WriteLine("dog");
+                    fileNew.WriteLine("mouse");
+                    fileNew.WriteLine("tea");
+                    fileNew.WriteLine("sun");
+                    fileNew.WriteLine("hello");
+                    fileNew.WriteLine("world");
+                    fileNew.WriteLine("code fellows");
+                    fileNew.WriteLine("pull request");
+                }
+            }
+        }
+    }
+}
