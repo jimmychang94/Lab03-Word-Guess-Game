7ba238141906bb3129de2f01f64a703c4d57524d
Jimmy Chang <jimmyfchang94@gmail.com>
2018-06-08 00:19:19 -0700

    adds reading a file capability

--- Lab03-Word-Guess-Game/Lab03-Word-Guess-Game/Program.cs
+++ Lab03-Word-Guess-Game/Lab03-Word-Guess-Game/Program.cs
@@ -11,6 +11,15 @@ namespace Lab03_Word_Guess_Game
             string path = "../../../storedWords.txt";
             Console.WriteLine("Hello World!");
             CreateFile(path);
+            try
+            {
+                string word = ReadAFile(path);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, something went wrong with reading the file.");
+            }
         }
 
         /// <summary>
@@ -26,15 +35,38 @@ namespace Lab03_Word_Guess_Game
                 {
                     fileNew.WriteLine("cat");
                     fileNew.WriteLine("dog");
-                    fileNew.WriteLine("mouse");
                     fileNew.WriteLine("tea");
                     fileNew.WriteLine("sun");
+                    fileNew.WriteLine("mouse");
                     fileNew.WriteLine("hello");
                     fileNew.WriteLine("world");
                     fileNew.WriteLine("code fellows");
                     fileNew.WriteLine("pull request");
+                    fileNew.WriteLine("supercalifragilisticexpialidocious");
                 }
             }
+        }
+
+        static string ReadAFile(string path)
+        {
+            try
+            {
+                string[] allWords = File.ReadAllLines(path);
+                int randNum = RandomNumberGenerator(allWords.Length);
+                string chosenWord = allWords[randNum];
+                return chosenWord;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        static int RandomNumberGenerator(int i)
+        {
+            Random number = new Random();
+            int randNum = number.Next(0, i);
+            return randNum;
         }
     }
 }
